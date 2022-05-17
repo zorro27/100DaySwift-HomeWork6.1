6cbe0a1b5565d395584beac790ff8a467965b1db
zorro27 <73430357+zorro27@users.noreply.github.com>
2022-05-17 21:51:48 +0300

    Initial Commit

---
+++ 100DaySwift HomeWork6.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  100DaySwift HomeWork6.1
+//
+//  Created by Роман Зобнин on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
